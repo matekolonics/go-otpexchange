bd25e9d8cd4e74dfcd2f9400b494b21f062f0170
mattee12 <mattee@mattee.net>
2020-06-02 11:27:37 +0200

    This version is for testing. It should be functional. If it passes the trial, then it is redy for use.

--- exchange.go
+++ exchange.go
@@ -6,20 +6,58 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"strconv"
 )
 
-func main() {
-	fmt.Println("Started.")
-	client := &http.Client{}
+type CURRENCY string
+
+const (
+	EUR CURRENCY = "EUR"
+	HUF CURRENCY = "HUF"
+	USD CURRENCY = "USD"
+	GBP CURRENCY = "GBP"
+)
+
+func convert(from, to CURRENCY, amount float64) (buy float64, sell float64) {
 	data, err := json.Marshal(map[string]string{
-		"exchangeType":   "CURRENCY",
-		"baseAmount":     "5",
-		"baseCurrency":   "EUR",
-		"resultCurrency": "HUF",
+		"exchangeType":   "FOREIGN_EXCHANGE",
+		"baseAmount":     fmt.Sprintf("%f", amount),
+		"baseCurrency":   string(from),
+		"resultCurrency": string(to),
 	})
 	if err != nil {
 		panic(err)
 	}
+	resp := sendRequest(data)
+	body, _ := ioutil.ReadAll(resp.Body)
+	var unm map[string]string
+	json.Unmarshal(body, &unm)
+	buy, err = strconv.ParseFloat(unm["resultAmount"], 64)
+	if err != nil {
+		panic(err)
+	}
+	data, err = json.Marshal(map[string]string{
+		"exchangeType":   "FOREIGN_EXCHANGE",
+		"baseAmount":     fmt.Sprintf("%f", 1.0),
+		"baseCurrency":   string(to),
+		"resultCurrency": string(from),
+	})
+	if err != nil {
+		panic(err)
+	}
+	resp = sendRequest(data)
+	body, _ = ioutil.ReadAll(resp.Body)
+	json.Unmarshal(body, &unm)
+	result, err := strconv.ParseFloat(unm["resultAmount"], 64)
+	if err != nil {
+		panic(err)
+	}
+	sell = amount * (1 / result)
+	return
+}
+
+func sendRequest(data []byte) *http.Response {
+	client := &http.Client{}
 	req, err := http.NewRequest("POST", "https://www.otpbank.hu/apps/exchangerate/api/exchangerate/exchange", bytes.NewBuffer(data))
 	if err != nil {
 		panic(err)
@@ -41,6 +79,5 @@ func main() {
 		panic(err)
 	}
 	defer resp.Body.Close()
-	body, _ := ioutil.ReadAll(resp.Body)
-	fmt.Println(string(body))
+	return resp
 }
